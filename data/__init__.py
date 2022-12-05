440fe4dd637686c6304597215fc2fe8ca0d405d0
zsolt.meszaros <zsolt.meszaros@lensa.com>
2022-12-05 14:49:03 +0100

    puzzle 5 data

--- data/__init__.py
+++ data/__init__.py
@@ -2,3 +2,4 @@ from .puzzle_1_data import calories
 from .puzzle_2_data import matches
 from .puzzle_3_data import rucksacks
 from .puzzle_4_data import pairs
+from .puzzle_5_data import base, orders
